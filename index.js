017b1752ad45a4a17d987cbe576022a14b05cb19
Jaume Ferre <168031749+JaumeINSRoquetes@users.noreply.github.com>
2025-04-22 19:45:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ let canvas_creat = false;
 let diagrama;
 let valors = [[],[]];
 function canvia_seccio(num_boto) {
-    if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
-    omple_llista();
-}
-    if (num_boto == 4) {
-    mapa.invalidateSize();
-    if (typeof geoID === "undefined") {    // si encara no s'han obtingut les dades de localització del dispositiu
-        navigator.geolocation.watchPosition(geoExit);    // inicia el seguiment de la localització del dispositiu
-    }
-}
-    if (num_boto == 6) {
-        mostra_diagrama();
-    }
     const menu = document.getElementById("menu");
     const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
     for (let i = 1; i < num_botons; i++) {
@@ -33,6 +21,18 @@ function canvia_seccio(num_boto) {
             boto.style.backgroundColor = "#338BF0";
             seccio.style.display = "none";    // s'oculten les seccions inactives
         }
+    }
+     if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
+    omple_llista();
+}
+    if (num_boto == 4) {
+    mapa.invalidateSize();
+    if (typeof geoID === "undefined") {    // si encara no s'han obtingut les dades de localització del dispositiu
+        navigator.geolocation.watchPosition(geoExit);    // inicia el seguiment de la localització del dispositiu
+    }
+}
+    if (num_boto == 6) {
+        mostra_diagrama();
     }
 }
 function inici_sessio() {
