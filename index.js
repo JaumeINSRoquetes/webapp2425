138c7dc631379c8742c4cf4834ac5f4d3ecf0a10
Jaume Ferre <168031749+JaumeINSRoquetes@users.noreply.github.com>
2025-04-22 20:32:17 +0200

    Update index05.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let validat = false;    // variable que permet saber si hi ha algun usuari validat
+let nom, contrasenya;
+let scriptURL = "https://script.google.com/macros/s/AKfycbxln8tQ7ZeR4DxEhFmN_nUYzcQ5zSprWGd_0gmD8aivRuzHywSMVNsS3ie8ojJR7E7x/exec"    // s'ha de substituir la cadena de text per la URL del script
+
 function canvia_seccio(num_boto) {
     const menu = document.getElementById("menu");
     const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
@@ -13,6 +17,64 @@ function canvia_seccio(num_boto) {
             boto.style.color = "white";    // colors dels botons de seccions inactives
             boto.style.backgroundColor = "#950E17";
             seccio.style.display = "none";    // s'oculten les seccions inactives
+        }
+    }
+}
+function inici_sessio() {
+    nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
+    contrasenya = document.getElementById("contrasenya").value;
+    let consulta = scriptURL + "?query=select&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
+    fetch(consulta)
+        .then((resposta) => {   // registres que contenen el nom d'usuari i contrasenya escrits per l'usuari
+            return resposta.json();    // conversió a llista
+        })
+        .then((resposta) => {
+            if(resposta.length == 0) {    // llista buida
+                window.alert("El nom d'usuari o la contrasenya no són correctes.");
+            }
+            else {    // llista amb (almenys) un registre
+                window.alert("S'ha iniciat correctament la sessió.");
+                inicia_sessio();    // usuari validat, s'executen les instruccions del procediment "inicia_sessio"
+            }
+        });    
+}
+function inicia_sessio() {
+    validat = true;    // usuari validat
+    document.getElementById("seccio_0").style.display = "none";    // s'oculta la secció de validació d'usuaris
+    canvia_seccio(1);    // es mostra la secció 1
+}
+function nou_usuari() {
+    nom = document.getElementById("nom_usuari").value;
+    contrasenya = document.getElementById("contrasenya").value;
+    let consulta_1 = scriptURL + "?query=select&where=usuari&is=" + nom;    // primera consulta per saber si ja existeix algun usuari amb el nom escrit per l'usuari que es vol registrar
+    fetch(consulta_1)
+        .then((resposta) => {
+            return resposta.json();
+        })
+        .then((resposta) => {
+            if(resposta.length == 0) {    // No hi ha cap altres usuari amb el mateix nom
+                let consulta_2 = scriptURL + "?query=insert&values=" + nom + "$$" + contrasenya;    // segona consulta per registrar l'usuari nou
+                fetch(consulta_2)
+                    .then((resposta) => {
+                        if (resposta.ok) {    // s'ha pogut afegir una registre en la base de dades
+                            window.alert("S'ha completat el registre d'usuari.")
+                            inicia_sessio();
+                        }
+                        else {    // no s'ha pogut afegir un registre en la base de dades
+                            alert("S'ha produït un error en el registre d'usuari.")
+                        }
+                    })
+            } 
+            else {    // l'usuari ha de tornar-ho a intentar amb un nom diferent
+                alert("Ja existeix un usuari amb aquest nom.");
+            }
+        });
+}
+function tanca_sessio() {
+    if (validat) {
+        if (confirm("Vols tancar la sessió?")) {    // S'ha respost "Sí"
+            storage.setItem("usuari", "");
+            location.reload();    // recàrrega de la pàgina, es reinicialitzen totes les variables
         }
     }
 }
